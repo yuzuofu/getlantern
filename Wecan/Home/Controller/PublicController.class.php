9386b2fdd40cf3baa37cf395e35635c000630a93
yuzuofu <15068232338@163.com>
2017-04-12 12:52:54 +0800

    网页授权测试

--- Wecan/Home/Controller/PublicController.class.php
+++ Wecan/Home/Controller/PublicController.class.php
@@ -15,7 +15,7 @@ class PublicController extends Controller
         ];
         $app = new Application($config);
         $oauth = $app->oauth;
-        var_dump(session('customer_openid'));exit();
+
         if (empty(session("customer_openid"))) {
             return $oauth->redirect();
         }
@@ -32,6 +32,7 @@ class PublicController extends Controller
         $app = new Application($config);
         $oauth = $app->oauth;
         $user = $oauth->user()->toArray();
+        var_dump($user);exit();
         //  TODO:将用户信息存储进数据库
         //  TODO:给用户发送优惠券
         session('customer_openid', $user->id);
