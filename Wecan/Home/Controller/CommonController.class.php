3c0da0074129ac8ff43bbead5bd8deac925d777f
yuzuofu <15068232338@163.com>
2017-04-12 14:23:58 +0800

    测试

--- Wecan/Home/Controller/CommonController.class.php
+++ Wecan/Home/Controller/CommonController.class.php
@@ -67,6 +67,7 @@ class CommonController extends Controller
         $app = new Application($config);
         $oauth = $app->oauth;
         $user = $oauth->user()->toArray();
+        var_dump($user);
         //  TODO:将用户信息存储进数据库
         //  TODO:给用户发送优惠券
         session('customer_openid', $user->id);
