57da0483902d47e8e7c95aa2c48a1f55fe58ab0b
yuzuofu <15068232338@163.com>
2017-04-13 18:22:44 +0800

    导航测试

--- Wecan/Home/Controller/CommonController.class.php
+++ Wecan/Home/Controller/CommonController.class.php
@@ -23,34 +23,35 @@ class CommonController extends Controller
         $server = $app->server;
         $server->setMessageHandler(function ($message) {
             $msg_type = $message->MsgType;
-            if ($msg_type == 'location') {//  存储用户地理位置
-                $user_locationTable = M('user_location');
-                $user_location = $user_locationTable
-                                ->where(['openid' => $message->FromUserName])
-                                ->getField('id');
-
-                if (!$user_location) {
-                    $data = [
-                        'openid' => $message->FromUserName,
-                        'latitude' => $message->Location_X,
-                        'longitude' => $message->Location_Y,
-                        'create_time' => time(),
-                        'update_time' => time()
-                    ];
-                    $user_locationTable->add($data);
-                } else {
-                    $data = [
-                        'id' => $user_location,
-                        'latitude' => $message->Location_X,
-                        'longitude' => $message->Location_Y,
-                        'update_time' => time()
-                    ];
-                    $user_locationTable->save($data);
-                }
-                return "地理位置更新成功";
-            } else {
-                return "您好！欢迎关注我！";
-            }
+            return $msg_type;
+//            if ($msg_type == 'location') {//  存储用户地理位置
+//                $user_locationTable = M('user_location');
+//                $user_location = $user_locationTable
+//                                ->where(['openid' => $message->FromUserName])
+//                                ->getField('id');
+//
+//                if (!$user_location) {
+//                    $data = [
+//                        'openid' => $message->FromUserName,
+//                        'latitude' => $message->Location_X,
+//                        'longitude' => $message->Location_Y,
+//                        'create_time' => time(),
+//                        'update_time' => time()
+//                    ];
+//                    $user_locationTable->add($data);
+//                } else {
+//                    $data = [
+//                        'id' => $user_location,
+//                        'latitude' => $message->Location_X,
+//                        'longitude' => $message->Location_Y,
+//                        'update_time' => time()
+//                    ];
+//                    $user_locationTable->save($data);
+//                }
+//                return "地理位置更新成功";
+//            } else {
+//                return "您好！欢迎关注我！";
+//            }
         });
         $response = $server->serve();
         $response->send();
